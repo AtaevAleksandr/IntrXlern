aaad08e311f9ab8229ccc346f30cf388e9cb8bf1
Aleksandr Ataev <onlyaa94@mail.ru>
2024-03-20 14:30:30 +0300

    Initial Commit

---
+++ IntrXlern/IntrXlernApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntrXlernApp.swift
+//  IntrXlern
+//
+//  Created by Aleksandr Ataev on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct IntrXlernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
